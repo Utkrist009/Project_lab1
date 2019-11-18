a915eee477ceffc0e54c22fe1e27bd2ce23e9c91
Utkrist009 <55606609+Utkrist009@users.noreply.github.com>
2019-11-18 02:48:26 -0600

    Add files via upload

--- SourceCode/Servo.v
+++ SourceCode/Servo.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10/08/2019 07:17:03 PM
+// Create Date: 11/11/2019 02:56:45 PM
 // Design Name: 
-// Module Name: servo_test
+// Module Name: Servo
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -21,18 +21,111 @@
 
 
 module Servo(
-input clk,
-input[5:0] sw,
-output enable
-    );
-    
-Servo_pwm Test(
+input clk, reset,
+//input[5:0] sw,
+output enable1,
+//output enable_temp,
+output enable2
+);
+wire timerDone;
+reg timerActive= 0;
+reg [3:0] Seconds = 4'b0000;
+
+
+wire n_clk;    
+
+localparam servo90d=18'd145000,servo180d=18'd65000,servo0d=18'd240000,servo30d=18'd200000,servo150d=18'd95000,servo135d = 18'd110000;
+
+reg [17:0] angle_value_servo1 = servo90d;
+reg [17:0] angle_value_servo2 = servo90d;
+//localparam servo90d=3,servo180d=6,servo0d=1,servo30d=2,servo150d=5,servo135d=4;
+
+
+// Second_Counter delay(.clk(clk),.reset(reset),.n_clk(n_clk));
+//Second_Counter delay(.clk(clk));
+ reg[2:0] Servo_FSM = 3'b000; 
+  
+Servo_PWM servo1(
 .clk(clk),
-.sw(sw),
-.angle(enable));
+.angle_value(angle_value_servo1),
+.angle(enable1));
+
+Servo_PWM servo2(
+.clk(clk),
+.angle_value(angle_value_servo2),
+.angle(enable2));
+
+delay_seconds test(.clk(clk), .limit(Seconds), .active(timerActive), .signal(timerDone));
+//localparam DELAY = 10000;
+//reg [DELAY-1:0] shift_reg;
+//always @(posedge clk)
+//    begin
+//          shift_reg <= {shift_reg[DELAY-2:0], enable_temp};
+//    end    
+//assign enable2 = shift_reg[DELAY-1];
+ 
+ always @(posedge clk)
+ begin
+     case (Servo_FSM)
+     3'b000:
+     begin 
+     angle_value_servo1 <= servo90d;
+     angle_value_servo2 <= servo90d;
+     Seconds = 2; //period set to 2M so 4sec delay
+     timerActive = 1;
+     if (timerDone)
+     begin
+       timerActive = 0;
+       Seconds = 0;
+       Servo_FSM = 3'b001;
+     end
+   end  
+   
+   3'b001:
+     begin 
+     angle_value_servo1 <= servo180d;
+     Seconds = 2;   //period set to 2M so 4sec delay
+     timerActive = 1;
+     if (timerDone)
+     begin
+       timerActive = 0;
+       Seconds = 0;
+       Servo_FSM = 3'b010;
+     end
+   end  
         
-    
+   3'b010:
+     begin 
+     angle_value_servo2 <= servo180d;
+     Seconds = 2;    //period set to 2M so 4sec delay
+     timerActive = 1;
+     if (timerDone)
+     begin
+       timerActive = 0;
+       Seconds = 0;
+       Servo_FSM = 3'b011;
+     end
+   end  
+   
+   3'b011:
+     begin 
+      angle_value_servo1 <= servo90d;
+     angle_value_servo2 <= servo90d;
+     Seconds = 2;    //period set to 2M so 4sec delay
+     timerActive = 1;
+     if (timerDone)
+     begin
+       timerActive = 0;
+       Seconds = 0;
+       Servo_FSM <= 3'b100;
+     end
+   end  
+    3'b100:
+     begin
+     //angle_value_servo1 = servo30d;
+     //angle_value_servo2 = servo30d;
+     end
+           
+ endcase      
+   end 
 endmodule
-
-
-
---
+++ SourceCode/delay_seconds.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/11/2019 02:57:48 PM
+// Design Name: 
+// Module Name: delay_seconds
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module delay_seconds(
+    input clk,
+    input[3:0] limit,
+    input active,
+    output reg signal = 0
+    );
+
+reg reset = 0;
+reg [31:0] counter = 0;
+reg [3:0] second_counter = 0;
+    reg [31:0] Period = 200000000;
+    
+always@ (posedge clk)
+begin
+
+  if(reset)
+   begin
+     counter = 0;
+     second_counter=0;
+     signal = 0;
+    end 
+    if (active)
+    begin   
+    //if ((second_counter<countTo)&&active)
+    if (second_counter<limit)
+    begin
+    reset = 0;
+    if (counter<Period)
+    begin
+    counter <= counter + 1;
+    end
+    else 
+    begin 
+    counter = 0;
+    second_counter <= second_counter + 1;
+    end
+   end
+   
+   else
+   begin 
+   signal = 1;
+   reset = 1; 
+   end
+  end
+ end
+endmodule
