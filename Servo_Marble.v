7c9c88ff34da1b106b6ad52743597ffb4daee8d9
itzel8itzy <55606616+itzel8itzy@users.noreply.github.com>
2019-11-26 16:23:58 -0600

    Add files via upload

--- Servo_Marble.v
+++ Servo_Marble.v
@@ -8,22 +8,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module Servo_Marble(
-    input clk,
-    input enable_servo_marble,
-    input marble,
-    output reg [20:0] angle_value,
-    output reg done_servo_marble
+    input             clk, rst,
+    input             enable_servo_marble,//from Main SM
+    input [1:0]       marble,
+    output reg [19:0] angle_value,
+    output reg        done_servo_marble // To Main SM 
    );
 
+   // State definitions
    localparam INIT_STATE = 0, STATE_1 = 1, STATE_2 = 2, STATE_3 = 3;
+   // Define the delay for waiting for the servo to be finished
+   //localparam DELAY=100;
+   localparam DELAY=50_000_000;
    
-   reg state;
+   reg [1:0] state;
    reg [1:0] count;
    reg [31:0] count_delay;
    reg [1:0]  marble_sync;
-
-   reg start_count;
-   reg done_count;
 
    initial
      begin
@@ -31,8 +32,6 @@ module Servo_Marble(
         state <= 0;
         count <= 0;
         count_delay <= 0;
-        start_count <= 0;
-        done_count <= 0;
         marble_sync <= 0;
         done_servo_marble <= 0;
      end
@@ -41,64 +40,57 @@ module Servo_Marble(
    always @ (posedge clk)
      marble_sync <= marble;
 
-   // Counter
-   always @ (posedge clk)
-     begin
-        if (start_count)
-          count_delay <= count_delay + 1;
-        // Half a second
-        if (count_delay >= 50_000_000)
-          done_count <= 1;
-     end
-
    // State machine
    always @(posedge clk)
      begin
-        if (enable_servo_marble)
-          case(state)
-            INIT_STATE:
-              begin
-                 count <= count+1;
-                 if(count<marble_sync)
-                   state <= STATE_2;
-                 else
-                   begin
-                      state <= INIT_STATE;
-                      done_servo_marble <= 1;
-                   end
-              end
-            STATE_2: 
-              begin
-                 angle_value <= 20'd230000;
-                 start_count <= 1; 
-                 if(done_count)
-                   begin
-                      start_count <= 0;
-                      state <= STATE_3;
-                   end
-              end
-
-            STATE_3: 
-              begin 
-                 angle_value <= 20'd65000;
-                 start_count <= 1; 
-                 if(done_count)
-                   begin
-                      start_count <= 0;
-                      state <= INIT_STATE;
-                   end
-              end
-            default: state<=INIT_STATE;
-          endcase // case (state)
-        else
+        if (rst)
           begin
-             done_servo_marble <= 0;
-             state <= INIT_STATE;
-             angle_value <= 0;
-             count <= 0;
-             count_delay <= 0;
-             start_count <= 0;
+             /*AUTORESET*/
+             // Beginning of autoreset for uninitialized flops
+             angle_value <= 20'h0;
+             count <= 2'h0;
+             count_delay <= 32'h0;
+             done_servo_marble <= 1'h0;
+             state <= 2'h0;
+             // End of automatics
           end  
+        else 
+          if (enable_servo_marble && ~done_servo_marble)
+            case(state)
+              INIT_STATE:
+                begin
+                   count <= count+1;
+                   if(count<=marble_sync)
+                     state <= STATE_2;
+                   else
+                     begin
+                        state <= INIT_STATE;
+                        done_servo_marble <= 1;
+                     end
+                end
+              STATE_2: 
+                begin
+                   angle_value <= 20'd230000;
+                   count_delay <= count_delay + 1;
+                   if(count_delay >= DELAY)
+                     begin
+                        count_delay <= 0;
+                        state <= STATE_3;
+                     end
+                end
+
+              STATE_3: 
+                begin 
+                   angle_value <= 20'd65000;
+                   count_delay <= count_delay + 1;
+                   if(count_delay >= DELAY)
+                     begin
+                        count_delay <= 0;
+                        state <= INIT_STATE;
+                     end
+                end
+              default: state<=INIT_STATE;
+            endcase // case (state)
      end // always @ (posedge clk)
 
 endmodule
